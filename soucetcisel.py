328311a21b7d7fc17706e9cfaf34724b31221b5c
Ivana Kubásková <ivanakubaskova@seznam.cz>
2018-04-17 16:18:01 +0200

    úkol č. 19 Napiš program , který načte 3 čísla a zjistí, zda je jejich součet větší než 10.

---
+++ soucetcisel.py
@@ -0,0 +1,8 @@
+a = int(input("Zadej 1. číslo"))
+b = int(input ("Zadej 2. číslo"))
+c = int(input ("Zadej 3. číslo"))
+soucet = a+b+c
+if soucet < 10:
+    print("True")
+else:
+    print("součet čísel je větší než 10")
